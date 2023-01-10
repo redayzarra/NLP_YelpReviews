b6bb70eb237ca6f374eb13634b25d861c1121dea
ReDay Zarra <redayzarra@gmail.com>
2023-01-10 16:51:19 -0500

    Adding project information and importing necessary libraries

---
+++ Yelp_Reviews_NLP.py
@@ -0,0 +1,21 @@
+"""
+Yelp Reviews Classification
+
+This project covers the topic of natural language processing or NLP to classify 
+user-generated text and determine their intent. The goal of this project is to build
+a model that can automatically classify Yelp reviews into one of several predefined
+categories, such as "positive," "negative," or "neutral." To accomplish this, the 
+project uses NLP techniques to process and analyze the text of the reviews.
+
+The model can then be used to predict the label for new, unseen reviews. This can 
+be especially useful for businesses, as it can help them identify patterns in 
+customer feedback and make improvements to their products or services.
+
+We will start by importing the necessary libraries for natural language processing:
+"""
+# Importing the necessary libraries
+import pandas as pd # Pandas is used for data frame manipulations
+import numpy as np # NumPy is a package used for numerical analysis
+
+import matplotlib.pyplot as plt
+import seaborn as sns
